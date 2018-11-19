4570003dd823fa00635e9e90cce6e56a9b1f3417
xzmeasy <1280428201@qq.com>
2018-11-19 16:03:04 +0800

    init commit

---
+++ src/main/java/com/java/cloud/springcloudsummary/SpringcloudSummaryApplication.java
@@ -0,0 +1,12 @@
+package com.java.cloud.springcloudsummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudSummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudSummaryApplication.class, args);
+    }
+}
---
+++ src/test/java/com/java/cloud/springcloudsummary/SpringcloudSummaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.java.cloud.springcloudsummary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudSummaryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
